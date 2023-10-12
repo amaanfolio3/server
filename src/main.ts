0f2ea0141c9e1b36ce617bf6f448058c97e7c39c
Amaan <amanullahjalal@folio3.com>
2023-10-12 19:33:23 +0500

    task - enabled cors and changed port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   setupSwagger(app);
-  await app.listen(3001);
+  await app.listen(5000);
 }
 bootstrap();
