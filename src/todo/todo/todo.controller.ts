fd32e7956a772ddacd0f7f8b7671a89c22a7e70d
Amaan <amanullahjalal@folio3.com>
2023-10-12 19:04:23 +0500

    init

---
+++ src/todo/todo/todo.controller.ts
@@ -0,0 +1,58 @@
+import {
+  Controller,
+  Get,
+  Body,
+  Post,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { TodoDTO } from './todo.dto';
+import { todos } from './todos.mock';
+
+let todosList: TodoDTO[] = todos;
+
+@ApiTags('todos')
+@Controller('todos') // here todos referring to localhost/todos
+export class TodoController {
+  @ApiOperation({ summary: 'Get all todos' })
+  @ApiResponse({ status: 200, description: 'Return all todos.' })
+  @Get()
+  getTodos(): TodoDTO[] {
+    return todosList;
+  }
+
+  @ApiOperation({ summary: 'Create todo' })
+  @ApiResponse({ status: 200, description: 'Create todo.' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @Post()
+  createTodo(@Body() todo: TodoDTO): TodoDTO[] {
+    todosList.push({ ...todo, id: todosList.length + 1 });
+    return todosList;
+  }
+
+  // swagger decorators for put and delete
+  @ApiOperation({ summary: 'Update todo' })
+  @ApiResponse({ status: 200, description: 'Update todo.' })
+  @ApiResponse({ status: 400, description: 'Bad Request' })
+  @Put(':id')
+  updateTodo(@Param('id') id: string, @Body() todo: TodoDTO): TodoDTO[] {
+    todosList = todosList.map((t) => {
+      console.log('>>> -> file: todo.controller.ts:47 -> t->', t);
+      if (t.id.toString() === id) {
+        return todo;
+      }
+      return t;
+    });
+    return todosList;
+  }
+
+  @ApiOperation({ summary: 'Delete todo' })
+  @ApiOperation({ description: 'Delete todo' })
+  @Delete(':id')
+  deleteTodoById(@Param('id') id: string): TodoDTO[] {
+    todosList = todosList.filter((t) => t.id.toString() !== id);
+    return todosList;
+  }
+}
